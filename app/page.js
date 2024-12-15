56ab670b8529fd4260d4da719e384f07f2770b04
tanzil <tanjilislam90@gmail.com>
2024-12-16 02:05:03 +0600

    section devided

--- app/page.js
+++ app/page.js
@@ -31,18 +31,29 @@ export default function Home() {
   }
   return (
     <div className="px-4 py-10 mx-auto">
-      <h1 className="text-3xl font-bold mb-4">
+      <h1 className="text-3xl font-bold mb-4 text-center mb-5">
         Next Gen Code Editor with Video Sync
       </h1>
       <div className="space-x-4 grid grid-cols-1 md:grid-cols-3 gap-4 ">
-        <VideoPlayer
-          url={'/html.mp4'}
-          onProgress={handleProgress}
-          onSeek={handleSeek}
-        />
-        <CodeEditor code={code} setCode={setCode} />
-        <div className="border p-4">
-          <CodePreview code={code} />
+        <div>
+          <p className="text-2xl font-semibold">Video</p>
+          <VideoPlayer
+            url={'/html.mp4'}
+            onProgress={handleProgress}
+            onSeek={handleSeek}
+          />
+        </div>
+
+        <div>
+          <p className="text-2xl font-semibold">Code Editor</p>
+          <CodeEditor code={code} setCode={setCode} />
+        </div>
+
+        <div className="h-full">
+          <p className="text-2xl font-semibold">Output</p>
+          <div className="border p-4 h-full">
+            <CodePreview code={code} />
+          </div>
         </div>
       </div>
     </div>
